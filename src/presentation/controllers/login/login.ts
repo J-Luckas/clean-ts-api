05eeee85423ca160ca9be20b53a8211c854bf29b
J-Luckas <jairoluckas@hotmail.com>
2022-08-31 02:16:00 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,30 +1,23 @@
-import { Authentication, EmailValidator, HttpRequest, Controller, HttpResponse } from './login-protocols'
+import { Authentication, HttpRequest, Controller, HttpResponse, Validation } from './login-protocols'
 import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
-import { InvalidParamError, MissingParamError } from '../../errors'
 
 export class LoginController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
-    private readonly authentication: Authentication
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['email', 'password']
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
       }
       const { email, password } = httpRequest.body
-      const isValidEmail = this.emailValidator.isValid(email)
-      if (!isValidEmail) {
-        return badRequest(new InvalidParamError('email'))
-      }
-
       const accessToken = await this.authentication.auth(email, password)
-      if (!accessToken) return unauthorized()
+      if (!accessToken) {
+        return unauthorized()
+      }
 
       return ok({ accessToken })
     } catch (error: any) {
